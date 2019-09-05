f7c63410e6540037cdce6bdec34bb04a969fe5eb
Mendez <alexis.mendezpvd@gmail.com>
2019-09-05 19:38:14 -0400

    convert celsius to fahrenheit

---
+++ basicalgorithms/convertcelsius.js
@@ -0,0 +1,7 @@
+function convertToF(celsius) {
+    var fahrenheit;
+    fahrenheit = (celsius * (9/5)) + 32; // fahenheit = celsius * 9/5 + 32 works too.
+    return fahrenheit;
+  }
+  // Change the inputs below to test your code
+  convertToF(30);
